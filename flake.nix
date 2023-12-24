6ddfba9d60e2e07e2ae56b6a34a44403e34aa620
Rich <richard.g.otap@gmail.com>
2023-12-24 11:11:59 -0600

    flake: Add nix flake.

---
+++ flake.nix
@@ -0,0 +1,192 @@
+{
+  description = "DT Fetcher";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    rust-overlay,
+    crane,
+    flake-utils,
+  }: let
+    overlays = [
+      (import rust-overlay)
+    ];
+
+    macFrameworks = pkgs: let
+      frameworks = pkgs.darwin.apple_sdk.frameworks;
+    in
+      with frameworks; [
+        CoreFoundation
+        CoreServices
+        Security
+        SystemConfiguration
+      ];
+  in
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [(import rust-overlay)];
+        };
+        rust = pkgs.rust-bin.stable.latest.default;
+        craneLib = (crane.mkLib pkgs).overrideToolchain rust;
+        overridableCrate = pkgs.lib.makeOverridable ({toolchain}: let
+          craneLib = (crane.mkLib pkgs).overrideToolchain toolchain;
+        in
+          craneLib.buildPackage {
+            pname = "dt-fetcher";
+            version = "0.1.0";
+            src = craneLib.cleanCargoSource (craneLib.path ./.);
+            strictDeps = true;
+            nativeBuildInputs = [
+              pkgs.pkg-config
+            ];
+            buildInputs =
+              [
+                pkgs.openssl
+                pkgs.sqlite
+              ]
+              ++ pkgs.lib.optionals pkgs.stdenv.isDarwin (macFrameworks pkgs);
+          }) {toolchain = rust;};
+        container = crate: {
+          name = crate.pname;
+          tag = "latest";
+          created = "now";
+          contents = [pkgs.cacert];
+          config = {
+            Labels = {
+              "org.opencontainers.image.source" = "https://github.com/capslock/dt-fetcher";
+              "org.opencontainers.image.description" = "DT Fetcher";
+              "org.opencontainers.image.licenses" = "MIT";
+            };
+            Entrypoint = ["${crate}/bin/${crate.name}"];
+          };
+        };
+      in {
+        packages = {
+          default = overridableCrate;
+          container = pkgs.lib.makeOverridable ({crate}: pkgs.dockerTools.buildLayeredImage (container crate)) {crate = overridableCrate;};
+          streamedContainer = pkgs.lib.makeOverridable ({crate}: pkgs.dockerTools.streamLayeredImage (container crate)) {crate = overridableCrate;};
+        };
+        checks = {inherit overridableCrate;};
+        apps.default = flake-utils.lib.mkApp {
+          drv = overridableCrate;
+        };
+        devShells.default = craneLib.devShell {
+          buildInputs = [pkgs.pkg-config pkgs.openssl.dev pkgs.sqlite];
+          checks = self.checks.${system};
+          packages = pkgs.lib.optionals pkgs.stdenv.isDarwin (
+            with pkgs;
+              [
+                libiconv
+              ]
+              ++ macFrameworks pkgs
+          );
+        };
+      }
+    )
+    // {
+      nixosModules.default = {
+        config,
+        lib,
+        pkgs,
+        ...
+      }: let
+        cfg = config.services.dtFetcher;
+        settingsFormat = pkgs.formats.toml {};
+      in
+        with lib; {
+          options = {
+            services.dtFetcher = {
+              enable = mkOption {
+                default = false;
+                type = with types; bool;
+                description = ''
+                  Start the dt fetcher.
+                '';
+              };
+              environmentFile = mkOption {
+                example = "./dtfetcher.env";
+                type = with types; nullOr str;
+                default = null;
+                description = ''
+                  File which contains environment settings for the dt-fetcher service.
+                '';
+              };
+              environment = mkOption {
+                example = "RUSTLOG=info";
+                type = with types; str;
+                default = "\"RUSTLOG=info,hyper=error\"";
+                description = ''
+                  Environment settings for the dt-fetcher service.
+                '';
+              };
+              package = mkOption {
+                type = types.package;
+                default = self.packages.${pkgs.system}.default;
+                defaultText = literalExpression "self.packages.$\{pkgs.system\}.default";
+                description = ''
+                  dt-fetcher package to use.
+                '';
+              };
+            };
+          };
+
+          config = mkIf cfg.enable {
+            systemd.services.dtFetcher = {
+              wantedBy = ["multi-user.target"];
+              after = ["network-online.target"];
+              description = "DT Fetcher";
+              serviceConfig = let
+                pkg = cfg.package;
+              in {
+                Type = "exec";
+                DynamicUser = true;
+                ExecStart = "${pkg}/bin/dt-fetcher --log-to-systemd";
+                EnvironmentFile = mkIf (cfg.environmentFile != null) cfg.environmentFile;
+                Environment = cfg.environment;
+                NoNewPrivileges = true;
+                PrivateTmp = true;
+                PrivateDevices = true;
+                DevicePolicy = "closed";
+                ProtectSystem = "strict";
+                ProtectHome = "read-only";
+                ProtectControlGroups = true;
+                ProtectKernelLogs = true;
+                ProtectKernelModules = true;
+                ProtectKernelTunables = true;
+                RestrictNamespaces = true;
+                RestrictAddressFamilies = ["AF_INET" "AF_INET6" "AF_UNIX"];
+                RestrictRealtime = true;
+                RestrictSUIDSGID = true;
+                LockPersonality = true;
+                CapabilityBoundingSet = [""];
+                ProcSubset = "pid";
+                ProtectClock = true;
+                ProtectProc = "noaccess";
+                ProtectHostname = true;
+                SystemCallArchitectures = "native";
+                SystemCallFilter = ["@system-service" "~@resources" "~@privileged"];
+                UMask = "0077";
+                PrivateUsers = true;
+                MemoryDenyWriteExecute = true;
+              };
+            };
+          };
+        };
+    };
+}
